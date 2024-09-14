f6abb111b15399b8fb7942ab34c1b5ed64bcb196
Alfiannnn13 <muhammadalfiannovanto17@gmail.com>
2024-09-14 15:14:17 +0700

    first commit2

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,14 +15,10 @@ const Projects = () => {
       </h1>
 
       <p className='text-slate-500 mt-2 leading-relaxed'>
-        I've embarked on numerous projects throughout the years, but these are
-        the ones I hold closest to my heart. Many of them are open-source, so if
-        you come across something that piques your interest, feel free to
-        explore the codebase and contribute your ideas for further enhancements.
-        Your collaboration is highly valued!
+      Kalem belom kelar ini bikinnya
       </p>
 
-      <div className='flex flex-wrap my-20 gap-16'>
+      {/* <div className='flex flex-wrap my-20 gap-16'>
         {projects.map((project) => (
           <div className='lg:w-[400px] w-full' key={project.name}>
             <div className='block-container w-12 h-12'>
@@ -61,7 +57,7 @@ const Projects = () => {
         ))}
       </div>
 
-      <hr className='border-slate-200' />
+      <hr className='border-slate-200' /> */}
 
       <CTA />
     </section>
